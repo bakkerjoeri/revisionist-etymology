5b0011c3f0be06dbe38a3b9b0f25e00177a09242
Joeri Bakker <bakkerjoeri@gmail.com>
2021-07-04 22:52:02 +0200

    Add a tiny markdown -> HTML generator

---
+++ export.mjs
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+import fs, { mkdirSync, watch } from 'fs';
+import { micromark } from 'micromark';
+import path from 'path';
+import prettier from 'prettier';
+
+const srcPath = './src';
+const exportPath = './web';
+const rulesetFilePath = path.join(srcPath, 'revisionist-etymology.md');
+
+if (process.argv.includes('--watch')) {
+	build();
+	console.log('Watching for file changes...');
+	watch('./src', { recursive: true }, (event) => {
+		if (event === 'change') {
+			build();
+		}
+	});
+} else {
+	build();
+}
+
+function clean() {
+	if (fs.existsSync(exportPath)) {
+		fs.rmSync(exportPath, { recursive: true });
+	}
+}
+
+function build() {
+	console.log('Building...');
+
+	clean();
+	mkdirSync(exportPath);
+
+	const rulesetContents = fs.readFileSync(rulesetFilePath);
+	const rulesetMarkedUp = micromark(rulesetContents, {allowDangerousHtml: true});
+
+	const rulesetPrefixMarkup = `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width,initial-scale=1.0">
+			<link rel="stylesheet" href="style.css">
+			<title>Revisionist Etymology</title>
+		</head>
+
+		<body>
+	`;
+
+	const rulesetPostfixMarkup = `
+		</body>
+	</html>
+	`;
+
+	const rulesetHtml = `${rulesetPrefixMarkup}${rulesetMarkedUp}${rulesetPostfixMarkup}`;
+	fs.writeFileSync(path.join(exportPath, 'index.html'), prettier.format(rulesetHtml, { parser: 'html' }));
+	fs.copyFileSync(path.join(srcPath, 'style.css'), path.join(exportPath, 'style.css'));
+
+	console.log('Build finished.');
+}
--- src/revisionist-etymology.md
+++ src/revisionist-etymology.md
@@ -1,14 +1,15 @@
 # Revisionist Etymology
+<p class="subtitle">by Ewoud & Joeri</p>
 
-## What do you need to play
+## ☞ What do you need to play
 * 3 or more people
 * Things to write on, like post-its or 3-by-5's.
 * Things to write with.
 * Optionally: a fun accessory for the linguist to wear
 
-## How to play
+## ☞ How to play
 1. Appoint someone as the linguist for this round.
-2. The linguist picks a word or phrase, which the others write down on their piece of paper.
+2. The linguist picks a word or phrase, which the others each write down on a piece of paper.
 3. The other players then make up the etymology of that word or phrase and write it down. They fold up their work and hand it to the linguist.
 4. The linguist shuffles the submissions and reads each of them aloud.
 5. Then it's time to pick a new etymology for the chosen word or phrase. How an etymology is judged is up to you. The whole group can discuss which etymology they find best, but in the end, the linguist has the final say.
